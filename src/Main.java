c48adbd56161ad61877c9241293671c055ddaf63
NastiaZe <anastasiazemskova4212gmail.com>
2021-01-19 21:48:29 +0300

    commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int cost = 1500;
+        int mile = 1;
+        int total = cost / 20;
+        System.out.println(total);
+
+    }
+}
+
